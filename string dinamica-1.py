c93ef696767bace6ea1ef6097e0d1aefa27ed6af
Talita Hanna Cabral <talitalinden@gmail.com>
2022-12-28 12:17:08 -0300

    string dinamica

--- string dinamica-1.py
+++ string dinamica-1.py
@@ -1,6 +1,7 @@
 
 nome = str(input("Digite o seu nome: "))
 idade = int(input("Digite a sua idade: "))
+profissao = str(input("Digite a sua profissão: "))
 
-print("Meu nome é %s e eu tenho %d anos" % (nome, idade))
+print("Meu nome é %s , eu tenho %d anos e sou %s" % (nome, idade, profissao))
 
