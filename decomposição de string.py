d7e5e988a5cbeaaefbfacfd137812554b28cddd7
Talita Hanna Cabral <talitalinden@gmail.com>
2022-12-28 14:48:20 -0300

    decomposicao de string

--- decomposição de string.py
+++ decomposição de string.py
@@ -3,3 +3,10 @@ teste = "Hoje faz calor em João Pessoa"
 
 
 print(teste [9:14])
+
+print(teste [18:29])
+
+calor = teste [9:14]
+joao_pessoa = teste [18:29]
+
+print("Essa é a estação do %s em %s " % (calor, joao_pessoa))
