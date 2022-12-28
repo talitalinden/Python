e7860ff980da14c834e2e48a187263c1a9e879c8
Talita Hanna Cabral <talitalinden@gmail.com>
2022-12-28 15:11:22 -0300

    decomposicao de string

--- decomposição de string.py
+++ decomposição de string.py
@@ -10,3 +10,7 @@ calor = teste [9:14]
 joao_pessoa = teste [18:29]
 
 print("Essa é a estação do %s em %s " % (calor, joao_pessoa))
+
+teste2 = "Nós estamos procurando o rubi na floresta"
+
+print(teste2 [25:29])
