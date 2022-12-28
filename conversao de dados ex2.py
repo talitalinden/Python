e07d3a8e66d01d1ca23f5c16f922a2ef2b14a0e4
Talita Hanna Cabral <talitalinden@gmail.com>
2022-12-28 15:53:12 -0300

    conversao de dados ex2

---
+++ conversao de dados ex2.py
@@ -0,0 +1,13 @@
+#faça um programa que receba um valor como salário;
+#faça um programa que receba a porcentagem de aumento;
+#exiba o valor total após o aumento no interpretador.
+
+salario = float(input("Digite o seu salário: "))
+aumento = int(input("Digite a sua porcentagem de aumento: "))
+
+
+novo_salario = salario + (salario * (aumento/100))
+
+print(novo_salario)
+
+
---
+++ conversao de dados.py
@@ -0,0 +1,8 @@
+#crie duas variáveis que vão receber números;
+#faça a conversão deles e insira em uma variável;
+#exiba a soma dos dois números 
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+print(num1 + num2)
