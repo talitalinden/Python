95684236f8557280b266cac7a67baf4e5c27b436
Talita Hanna Cabral <talitalinden@gmail.com>
2022-12-28 14:06:11 -0300

    exer string com float

---
+++ exer float string.py
@@ -0,0 +1,9 @@
+#crie tres variáveis: poupança, juros, fatura_cartao;
+#exiba os três valores na mesma string utilizando a forma dinâmica
+#como se fosse a mensagem de um app de banco.
+
+poupanca = 845.35
+juros = 0.01
+fatura_cartao = 524.15
+
+print("O valor da sua fatura é R$ %.2f , seu valor disponível é de R$ %.2f, com rendimanto de R$ %.2f ao mês." % (fatura_cartao, poupanca, juros))
