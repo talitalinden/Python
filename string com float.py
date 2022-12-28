890dfc22685dbcc1ace1134ed4a423c5e850717c
Talita Hanna Cabral <talitalinden@gmail.com>
2022-12-28 12:49:30 -0300

    string com float

--- string com float.py
+++ string com float.py
@@ -2,3 +2,9 @@
 pi = 3.14392472378
 
 print("O valor de pi é %.2f" % pi)
+
+print("***************")
+
+dinheiro = float(input("Digite quanto dinheiro você tem R$: "))
+
+print("Você tem R$ %.2f" % dinheiro)
