aea5884cc2babe94d7e0ce761d366a80ee13a040
Talita Hanna Cabral <talitalinden@gmail.com>
2022-12-28 16:26:27 -0300

    exercício 17

---
+++ exercicio 17.py
@@ -0,0 +1,9 @@
+#escreva um programa que recebe uma base e uma potência;
+#exiba o resultado
+
+base = int(input("Digite a base: "))
+potencia = int(input("Digite a potência: "))
+
+resultado = base ** potencia # para calcular potência de um número em python
+
+print("O resultado é %d" % resultado)
