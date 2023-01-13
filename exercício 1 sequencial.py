a6d2b48791b6bd7f8f87d3a9f0c3803f275f20f9
Talita Hanna Cabral <talitalinden@gmail.com>
2023-01-13 15:38:58 -0300

    exercício1

---
+++ exercício 1 sequencial.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça dois números e imprima a soma. 
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+soma = num1 + num2
+
+print("A soma dos números é %d" % soma)
